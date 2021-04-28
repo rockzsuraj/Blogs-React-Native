bec501234ce325653a630393623d891ccb65f9fe
rockzsuraj <surajkmr012@gmail.com>
2021-04-28 23:35:42 +0530

    second commit

---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,15 @@
+import React, {useContext} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import { Context } from '../context/BlogContext';
+
+const CreateScreen =({ navigation }) => {
+return <View>
+   <Text>
+    Create Screen
+    </Text>
+</View>
+};
+
+const Styles = StyleSheet.create({});
+
+export default CreateScreen;

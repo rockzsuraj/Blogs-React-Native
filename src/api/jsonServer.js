ece9febc26ba93a6d2ae1da70eebe45fbf4edc99
rockzsuraj <surajkmr012@gmail.com>
2021-08-06 16:31:10 +0530

    new commit

---
+++ src/api/jsonServer.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export default axios.create({
+  baseURL: "http://2ef06b89ec7f.ngrok.io",
+});
--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,7 +1,9 @@
 import createDataContext from './createDataContext';
-
+import jsonServer from '../api/jsonServer';
 const blogReducer = (state, action) => {
   switch (action.type) {
+    case 'get_blogposts': 
+    return action.payload;
     case 'edit_blogpost':
      return state.map((blogPost)=> {
        return blogPost.id === action.payload.id ? action.payload : blogPost;  
@@ -22,9 +24,18 @@ const blogReducer = (state, action) => {
   }
 };
 
+const getBlogPosts = dispatch => {
+return async () => {
+const response = await jsonServer.get('/blogposts');
+dispatch({type: 'get_blogposts', payload: response.data});
+}
+};
+
+
 const addBlogPost = dispatch => {
-  return (title, content, callback) => {
-    dispatch({ type: 'add_blogpost', payload: { title, content } });
+  return async (title, content, callback) => {
+    await jsonServer.post('/blogposts', {title, content});
+    //dispatch({ type: 'add_blogpost', payload: { title, content } });
     if(callback){
       callback();
     }
@@ -49,6 +60,6 @@ const editBlogPost = dispatch => {
 
 export const { Context, Provider } = createDataContext(
   blogReducer,
-  { addBlogPost, deleteBlogPost, editBlogPost},
-  [{ title: 'TEST POST', content: 'TEST CONTENT', id: 1 }]
+  { addBlogPost, deleteBlogPost, editBlogPost, getBlogPosts},
+  []
 );
